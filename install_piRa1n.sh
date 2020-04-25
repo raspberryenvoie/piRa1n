8b0ae27c978eea2a2054b694899b37bc5efc4f4c
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-04-25 16:21:29 +0200

    Update install_piRa1n.sh

--- install_piRa1n.sh
+++ install_piRa1n.sh
@@ -3,6 +3,7 @@ echo 'Installing piRa1n...make sure you have a working internet connection.'
 wget https://assets.checkra.in/downloads/linux/cli/arm/1ed46fb34c72ee942a1af185fd5347b999f4bba667d594dd56554e3b3d3ea417/checkra1n-armel -O /home/pi/piRa1n/piRa1n #Download Checkra1n
 sudo chmod +x /home/pi/piRa1n/piRa1n #Make script executable
 sudo chmod +x /home/pi/piRa1n/piRa1n.sh #Make script executable
+sudo chmod +x /home/pi/piRa1n/update_checkra1n.sh #Make script executable
 #The following commands will enable piRa1n at startup
 echo ' [Unit]
  Description=Checkra1n

37a8ce5e5b699518b8a9f30e335796d5fb5e3091
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-04-25 22:23:56 +0200

    Add files via upload

---
+++ uninstall_piRa1n.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+read -r -p "Do you really want to remove piRa1n ? [Y/n] " input
+case $input in
+    [yY][eE][sS]|[yY])
+    echo 'Removing piRa1n...'
+    sudo systemctl stop piRa1n.service
+    sudo systemctl disable piRa1n.service
+    sudo rm -rf /home/pi/piRa1n/ /lib/systemd/system/piRa1n.service
+    sudo systemctl daemon-reload
+    echo 'piRa1n has been removed.' ;;
+    [nN][oO]|[nN])
+ echo "piRa1n won't be removed."
+       ;;
+    *)
+ echo "Invalid input..."
+ exit 1
+ ;;
+esac

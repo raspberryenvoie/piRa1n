9eaa7b9ac35b3bd35acb905c9fad93c7e7798d2c
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-04-25 13:40:09 +0200

    Update update_checkra1n.sh

--- update_checkra1n.sh
+++ update_checkra1n.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-read -p 'Make sure you have a working internet connection.\nGo to checkra.in/releases/#all-downloads, copy and past here the download link for Linux (CLI, arm) and hit enter: ' link
+echo 'Make sure you have a working internet connection.'
+read -p 'Go to checkra.in/releases/#all-downloads, copy and past here the download link for Linux (CLI, arm) and hit enter: ' link
 echo 'Updating Checkra1n...'
 wget $link -O /home/pi/piRa1n/piRa1n
 echo 'Done!'

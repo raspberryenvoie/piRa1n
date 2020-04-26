b69452725ecc3c32a0a4b210cb509fd67332bf25
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-04-26 11:00:09 +0200

    Add files via upload

--- piRa1n_automatic_shutdown.sh
+++ piRa1n_automatic_shutdown.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+echo 'Restoring the default behavior...'
+sudo service piRa1n stop
+echo '#!/bin/sh
 sudo /home/pi/piRa1n/piRa1n -c -V -E
-sudo /sbin/shutdown now
+sudo /sbin/shutdown now' > piRa1n.sh
+sudo service piRa1n start
+echo 'Done!'
--- piRa1n_no_automatic_shutdown.sh
+++ piRa1n_no_automatic_shutdown.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
+##!/bin/sh
+echo 'piRa1n will be automatically restart after every Jailbreak and will not shutdown the Pi'
+sudo service piRa1n stop
+echo '#!/bin/sh
 while true
 do
   sudo /home/pi/piRa1n/piRa1n -c -V -E
-done
+done' > piRa1n.sh
+sudo service piRa1n start
+echo 'Done!'

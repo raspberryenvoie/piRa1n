e5167d3c120979d72bf172b61731ac9d08526027
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-14 14:10:12 +0200

    Update piRa1n_no_automatic_shutdown.sh

--- piRa1n_no_automatic_shutdown.sh
+++ piRa1n_no_automatic_shutdown.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo 'piRa1n will be automatically restart after every Jailbreak and will not shutdown the Pi'
 sudo service piRa1n stop
-echo '#!/bin/sh
+echo "#!/bin/sh
 while true
 do
-  sudo /home/pi/piRa1n/piRa1n -c -V -E
-done' > piRa1n.sh
+  sudo $HOME/piRa1n/piRa1n -c -V -E
+done" > piRa1n.sh
 sudo service piRa1n start
 echo 'Done!'

7c769ece24cedccef200d556c06a8f951e7fe7c8
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-14 12:32:40 +0200

    Update install_piRa1n.sh

--- install_piRa1n.sh
+++ install_piRa1n.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install usbmuxd -y
 wget https://assets.checkra.in/downloads/linux/cli/arm/1ed46fb34c72ee942a1af185fd5347b999f4bba667d594dd56554e3b3d3ea417/checkra1n-armel -O /home/pi/piRa1n/piRa1n #Download Checkra1n
-cd /home/pi/piRa1n/
+cd $HOME/piRa1n/
 sudo chmod +x piRa1n piRa1n.sh update_checkra1n.sh uninstall_piRa1n.sh piRa1n_no_automatic_shutdown.sh piRa1n_automatic_shutdown.sh piRa1n_safe_mode.sh #Make scripts executable
 #The following commands will enable piRa1n at startup
 echo ' [Unit]
@@ -20,5 +20,5 @@ sudo chmod 644 /lib/systemd/system/piRa1n.service
 sudo systemctl daemon-reload
 sudo systemctl enable piRa1n.service
 echo 'Done! Raspbian will now reboot.\nTo jailbreak your iDevice, put it in DFU mode and plug it to the Pi.\nEnjoy!'
-sleep 4
+sleep 5
 sudo reboot #Reboot Pi

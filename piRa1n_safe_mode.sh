172bb082a353823a84d56f45c802e8b2bdcbe508
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-17 22:47:36 +0200

    Update piRa1n_safe_mode.sh

--- piRa1n_safe_mode.sh
+++ piRa1n_safe_mode.sh
@@ -2,7 +2,7 @@
 echo 'piRa1n will boot your device in safe mode'
 sudo service piRa1n stop
 echo "#!/bin/sh
-sudo $HOME/piRa1n/piRa1n -c -V -s
+sudo $HOME/piRa1n/piRa1n -c -s
 sudo /sbin/shutdown now" > piRa1n.sh
 sudo service piRa1n start
 echo 'Changes were successfully applied!'

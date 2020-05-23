7095ce609c6af419adff740e0803d435b046197b
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-23 15:35:14 +0200

    Update update_checkra1n.sh

--- update_checkra1n.sh
+++ update_checkra1n.sh
@@ -3,10 +3,10 @@ echo 'Make sure you have a working internet connection.'
 read -p 'Go to checkra.in/releases/#all-downloads, copy and past here the download link for Linux (CLI, arm) and hit enter: ' link
 if [[ $link == *"https://assets.checkra.in/downloads/linux/cli/arm/"* ]]; then
   echo 'Updating Checkra1n...'
-  sudo service piRa1n stop
+  sudo systemctl stop piRa1n.service
   wget $link -O $HOME/piRa1n/piRa1n
   chmod +x $HOME/piRa1n/piRa1n
-  sudo service piRa1n start
+  sudo systemctl start piRa1n.service
   echo 'Done!'
 else
   echo 'Invalid URL'

e7943aa1ee02671cce018ad6f0e65f2bf3bfd474
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-27 14:21:18 +0200

    Update install_piRa1n.sh

--- install_piRa1n.sh
+++ install_piRa1n.sh
@@ -3,8 +3,10 @@
 echo 'Installing piRa1n...make sure you have a working internet connection.'
 sudo apt update
 sudo apt upgrade -y
-sudo apt install usbmuxd -y
-cd /home/pi/piRa1n/
+sudo apt install git usbmuxd -y
+cd /home/pi/
+git clone https://github.com/raspberryenvoie/piRa1n.git
+cd piRa1n/
 wget https://assets.checkra.in/downloads/linux/cli/arm/dde0ee4255403a427636bb76e09e409487f8be128af4b7d89fac78548bd5b35a/checkra1n -O piRa1n #Download Checkra1n
 chmod +x piRa1n #Make piRa1n executable
 #The following commands will enable piRa1n at startup

2f7833c349de88c82d447b876926001cff20b9dc
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-27 12:35:24 +0200

    Delete piRa1n_automatic_shutdown.sh

--- piRa1n_automatic_shutdown.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo 'Restoring the default behavior...'
-sudo service piRa1n stop
-echo "#!/bin/sh
-sudo $HOME/piRa1n/piRa1n -c -E
-sudo /sbin/shutdown now" > piRa1n.sh
-sudo service piRa1n start
-echo 'Done!'

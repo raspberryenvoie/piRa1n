cb13d7b238146a68730650991cc58940e5a6b584
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-27 18:34:07 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,5 +7,5 @@ cd piRa1n/
 wget https://assets.checkra.in/downloads/linux/cli/arm/dde0ee4255403a427636bb76e09e409487f8be128af4b7d89fac78548bd5b35a/checkra1n -O piRa1n #Download Checkra1n
 sudo chmod +x piRa1n
 sudo chown -R pi:pi /home/pi/piRa1n/
-sudo systemctl restart piRa1n.service
+sudo systemctl start piRa1n.service
 echo 'Update completed!'

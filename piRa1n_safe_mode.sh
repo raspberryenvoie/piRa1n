16f1e024aad19394c7f02ebc6a17762e2dda094f
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-27 12:36:30 +0200

    Delete piRa1n_safe_mode.sh

--- piRa1n_safe_mode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo 'piRa1n will boot your device in safe mode'
-sudo service piRa1n stop
-echo "#!/bin/sh
-sudo $HOME/piRa1n/piRa1n -c -E -s
-sudo /sbin/shutdown now" > piRa1n.sh
-sudo service piRa1n start
-echo 'Changes were successfully applied!'

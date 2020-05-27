757cf211248180fa1443b95c0c82c179fe02b60e
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-27 12:55:26 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo systemctl stop piRa1n.service
-rm -rf replaceMeByHomeDirectory/piRa1n/
+cd replaceMeByHomeDirectory/
+rm -rf piRa1n/
 git clone https://github.com/raspberryenvoie/piRa1n.git
 wget https://assets.checkra.in/downloads/linux/cli/arm/dde0ee4255403a427636bb76e09e409487f8be128af4b7d89fac78548bd5b35a/checkra1n -O replaceMeByHomeDirectory/piRa1n/piRa1n #Download Checkra1n
 sudo chmod +x replaceMeByHomeDirectory/piRa1n/piRa1n

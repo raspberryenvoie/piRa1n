a9b5416bd1bf936b8a5a71bd81b2817f63108cfd
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-07-04 17:01:56 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,45 +3,54 @@ while true; do
   read -r -p "Auto shutdown [Y/n]: " input
   case $input in
       [yY][eE][sS]|[yY])
-      sudo systemctl stop piRa1n.service
-      echo "#!/bin/sh
+        sudo systemctl stop piRa1n.service
+        echo "#!/bin/sh
 sudo /home/pi/piRa1n/piRa1n -c -E
 sudo /sbin/shutdown now" > /home/pi/piRa1n/piRa1n.sh
-      break ;;
+        break
+        ;;
       [nN][oO]|[nN])
-      sudo systemctl stop piRa1n.service
-      echo "#!/bin/sh
+        sudo systemctl stop piRa1n.service
+        echo "#!/bin/sh
 while true
 do
   sudo /home/pi/piRa1n/piRa1n -c -E
 done" > /home/pi/piRa1n/piRa1n.sh
-      break ;;
+        break 
+        ;;
       *)
-      echo "Invalid input..." ;;
+        echo "Invalid input..."
+        ;;
   esac
 done
 while true; do
   read -r -p "Safe mode [Y/n]: " input
   case $input in
       [yY][eE][sS]|[yY])
-      sed -i 's/^\(.*sudo \/home\/pi\/piRa1n\/piRa1n -c -E\).*$/& -s/g' /home/pi/piRa1n/piRa1n.sh # Add -s argument for safe mode
-      break ;;
+        sed -i 's/^\(.*sudo \/home\/pi\/piRa1n\/piRa1n -c -E\).*$/& -s/g' /home/pi/piRa1n/piRa1n.sh # Add -s argument for safe mode
+        break
+        ;;
       [nN][oO]|[nN])
-      break ;;
+        break
+        ;;
       *)
-      echo "Invalid input..." ;;
+        echo "Invalid input..."
+        ;;
   esac
 done
 while true; do
   read -r -p "Verbose boot [Y/n]: " input
   case $input in
       [yY][eE][sS]|[yY])
-      sed -i 's/^\(.*sudo \/home\/pi\/piRa1n\/piRa1n -c -E\).*$/& -V/g' /home/pi/piRa1n/piRa1n.sh # Add -v argument for verbose boot
-      break ;;
+        sed -i 's/^\(.*sudo \/home\/pi\/piRa1n\/piRa1n -c -E\).*$/& -V/g' /home/pi/piRa1n/piRa1n.sh # Add -v argument for verbose boot
+        break
+        ;;
       [nN][oO]|[nN])
-      break ;;
+        break
+        ;;
       *)
-      echo "Invalid input..." ;;
+        echo "Invalid input..."
+        ;;
   esac
 done
 sudo systemctl start piRa1n.service

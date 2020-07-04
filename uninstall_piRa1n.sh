bf0caa183371a3d30d04121e8d1ff908fb460669
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-07-04 19:22:50 +0200

    Update uninstall_piRa1n.sh

--- uninstall_piRa1n.sh
+++ uninstall_piRa1n.sh
@@ -2,16 +2,19 @@
 read -r -p "Do you really want to remove piRa1n? [Y/n] " input
 case $input in
     [yY][eE][sS]|[yY])
-    echo 'Removing piRa1n...'
-    sudo systemctl stop piRa1n.service
-    sudo systemctl disable piRa1n.service
-    sudo rm -rf /etc/systemd/system/piRa1n.service
-    sudo systemctl daemon-reload
-    rm -rf /home/pi/piRa1n/
-    echo 'piRa1n has been removed.' ;;
+        echo 'Removing piRa1n...'
+        sudo systemctl stop piRa1n.service
+        sudo systemctl disable piRa1n.service
+        sudo rm -rf /etc/systemd/system/piRa1n.service
+        sudo systemctl daemon-reload
+        rm -rf /home/pi/piRa1n/
+        echo 'piRa1n has been removed.'
+        ;;
     [nN][oO]|[nN])
-    echo "piRa1n won't be removed." ;;
+        echo "piRa1n won't be removed."
+        ;;
     *)
-    echo "Invalid input..."
-    exit 1 ;;
+        echo "Invalid input..."
+        exit 1
+        ;;
 esac

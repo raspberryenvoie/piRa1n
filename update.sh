153468686edd4cc7ac509d7c0f64882d4a0e7052
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-07-15 20:25:52 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -25,7 +25,7 @@ if [ $? -eq 0 ]; then
   sudo git clone https://github.com/raspberryenvoie/piRa1n.git
   sudo mv /home/pi/piRa1n.sh.backup /home/pi/piRa1n/piRa1n.sh
   cd piRa1n/
-  sudo wget https://assets.checkra.in/downloads/linux/cli/arm/dde0ee4255403a427636bb76e09e409487f8be128af4b7d89fac78548bd5b35a/checkra1n -O piRa1n # Download Checkra1n
+  sudo wget https://assets.checkra.in/downloads/linux/cli/arm/dde0ee4255403a427636bb76e09e409487f8be128af4b7d89fac78548bd5b35a/checkra1n -O checkra1n # Download Checkra1n
   sudo chmod +x piRa1n
   sudo chown -R pi:pi /home/pi/piRa1n/
   sudo chmod -R 755 /home/pi/piRa1n/
